543146cd40875a0f8006e9b96296c5a878bd6bdc
Rebeca <rebecagabrielly113@gmail.com>
2025-04-14 11:47:57 -0300

    Exercicios FOR

---
+++ exemplo2.js
@@ -0,0 +1,8 @@
+let numero = 0;
+console.log("números pares de 0 a 100:");
+while (numero <=100) {
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+    numero++
+}
---
+++ exemplo9.js
@@ -0,0 +1,7 @@
+let fatorial = 1;
+let N = 6;
+
+for (let i = N; i >= 1; i--) {
+    fatorial *= i;
+}
+console.log(`O fatorial de ${N} è: ${fatorial}`);
